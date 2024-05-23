ad1b4afa0825e085a4c10cfb8be4ec3a7c9db2b0
Chris Georgen <c-georgen@users.noreply.github.com>
2024-05-22 22:06:06 -0500

    untracking config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-export const ALCHEMY = "https://eth-sepolia.g.alchemy.com/v2/JkctVuOpjyJ1-kGNljFKQAcu53KImLYp"
-
-export const key = "0x2f451faed02153c54b5a9af8de6da6962565f3f05fed4d0b1d646c6d5c7e5e6e"
-
